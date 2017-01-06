4f89a0b002a6c75dd16e24f79d2c6b5ad9fc2375
shrutigoel911 <manasgaur@wl-10-1-1-184.wright.edu>
2017-01-06 15:53:46 -0500

    First commit

---
+++ HW4-Q2-3.R
@@ -0,0 +1,8 @@
+#HW4 of data mining
+data1 <- c(1, 1, 20, 3, 3, 6, 7, 7, 7, 8, 3, 8, 8, 8, 12, 15, 15, 16, 18, 6 )
+h<-hist(data1,breaks=10)
+h<-hist(data1,breaks=5)
+dataset1 <- c(1,1,20,3,3,6,7,7,7,8,3,8,8,8,12,15,15,16,18,6)
+dataset2 <- c(9,10,11,12,12,15,16,16,16,17,17,18,18,18,18,22,25, 25, 26, 28)
+boxplot(dataset1)
+boxplot(dataset2)
